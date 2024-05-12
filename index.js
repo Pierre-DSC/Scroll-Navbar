f9d42b8e013b463c91d88bc865df747a83d4635f
Pierre <descormes.pierre22@gmail.com>
2024-05-12 22:42:08 +0200

    last commit

--- index.js
+++ index.js
@@ -5,3 +5,21 @@
 // Stocker la valeur du précédent niveau de scroll pour savoir si l'on est monté ou descendu
 
 // Connaitre niveau de scroll (window.scrollY)
+let lastScroll = 0;
+const navbar = document.querySelector('.navbar');
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY < 200) {
+    // Scrolling within the first 200 pixels
+    navbar.style.top = 0;
+  } else {
+    // Scrolling past the first 200 pixels
+    if (window.scrollY < lastScroll) {
+      navbar.style.top = 0;
+    } else {
+      navbar.style.top = "-60px";
+    }
+  }
+
+  lastScroll = window.scrollY;
+});
